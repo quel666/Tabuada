71883aa14cb96e9eaada5bc1cc4b58edb6c31c53
Raquel <168922095+quel666@users.noreply.github.com>
2024-11-25 20:29:58 -0300

    Código-Refaturado

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
+
+/* var inicial = 0
+while(inicial <=0){
+    alert(inicial)
+    
+    inicial++ 
+    }
+    */
+   
+   /* var inicial = 0
+   do {
+    alert(inicial)
+    inicial++
+    
+    } while (inicial <= 10) */
+    
 const numero = document.querySelector ("#numero_usuario")
 const btn_tabuada = document.querySelector ("#btn_tabuada")
-const resultado = document.querySelector ("#res")
+const resultado = document.querySelector ("#res") 
 
-function descobrir_tabuada(){
-    const numero_convertido = Number (numero.value)
-    resultado.innerHTML = `
-    <p>${numero_convertido} x 1 = ${numero_convertido * 1}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 2}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 3}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 4}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 5}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 6}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 7}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 8}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 9}</p>
-    <p>${numero_convertido} x 1 = ${numero_convertido * 10 }</p>`
+function resultado_calculo (){
+    resultado.innerHTML = ``
+
+    var numero_convertido = Number(numero.value)
+
+    var inicial = 1 
+    while(inicial <= 10){
+
+        resultado.innerHTML += `<p> ${numero_convertido} x ${inicial} = ${numero_convertido * inicial} </p>`
+
+        inicial++
+
+    }
 }
-btn_tabuada.addEventListener ("click",descobrir_tabuada)
+
+btn_tabuada.addEventListener ("click",resultado_calculo)
+    
